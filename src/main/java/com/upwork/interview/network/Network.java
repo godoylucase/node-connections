0d9d7d5b55ce103c5073cef6a2cf0f472b578ee6
Lucas Godoy <godoy.lucas.e@gmail.com>
2018-08-02 13:05:07 -0300

    Initial commit

---
+++ src/main/java/com/upwork/interview/network/Network.java
@@ -0,0 +1,13 @@
+package com.upwork.interview.network;
+
+public class Network {
+
+    public void connect(int origin, int destination) {
+
+    }
+
+    public boolean query(int origin, int destination) {
+        return false;
+    }
+
+}

9230090e13b5fa79f5d83f0a842e3ae7386a1b44
Lucas Godoy <lucas.godoy@harmoney.co.nz>
2018-08-03 14:48:28 -0300

    Solution/observer (#1)
    * Initial commit
    
    * Full implemented basic logic

---
+++ src/main/java/com/upwork/interview/network/Node.java
@@ -0,0 +1,88 @@
+package com.upwork.interview.network;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Node implements PropertyChangeListener {
+
+    private int id;
+    private final Set<Node> connectedNodes;
+
+    private PropertyChangeSupport propertyChangeSupport;
+
+    public Node(int id) {
+        this.id = id;
+        this.connectedNodes = new HashSet<>();
+        propertyChangeSupport = new PropertyChangeSupport(this);
+    }
+
+    public void connectNode(Node toNode) {
+        connectNode(toNode, false);
+    }
+
+    public void connectNode(Node toNode, boolean isCallback) {
+        addNode(toNode);
+        NodeChangeSet nodeChangeSet = new NodeChangeSet(this, toNode, connectedNodes, isCallback);
+        PropertyChangeEvent event = new PropertyChangeEvent(this, "nodeChangeSet", null, nodeChangeSet);
+        propertyChangeSupport.firePropertyChange(event);
+    }
+
+    private void addNode(Node node) {
+        this.connectedNodes.add(node);
+        addPropertyChangeListener(node);
+    }
+
+    private void addPropertyChangeListener(PropertyChangeListener pcl) {
+        this.propertyChangeSupport.addPropertyChangeListener(pcl);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        NodeChangeSet changeSet = (NodeChangeSet) event.getNewValue();
+
+        Set<Node> nodes = changeSet.getSourcesConnections()
+                .parallelStream()
+                .filter(node -> !node.equals(this))
+                .collect(Collectors.toSet());
+
+        if (changeSet.getDestination().equals(this)) {
+            if (changeSet.isCallback()) {
+                nodes.forEach(this::addNode);
+            } else {
+                if (!this.connectedNodes.containsAll(nodes)) {
+                    nodes.forEach(this::addNode);
+                }
+                connectNode(changeSet.getSource(), true);
+            }
+        } else {
+            if (changeSet.isCallback()) {
+                nodes.forEach(this::addNode);
+            } else {
+                addNode(changeSet.getDestination());
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return id == node.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Set<Node> getConnectedNodes() {
+        return connectedNodes;
+    }
+
+}
---
+++ src/main/java/com/upwork/interview/network/NodeChangeSet.java
@@ -0,0 +1,47 @@
+package com.upwork.interview.network;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NodeChangeSet {
+
+    private Node source;
+    private Node destination;
+    private final Set<Node> sourcesConnections;
+    private boolean callback;
+
+    public NodeChangeSet(Node source, Node destination, Set<Node> sourcesConnections, boolean callback) {
+        this.source = source;
+        this.destination = destination;
+        this.sourcesConnections = new HashSet<>(sourcesConnections);
+        this.callback = callback;
+    }
+
+    public Node getSource() {
+        return source;
+    }
+
+    public void setSource(Node source) {
+        this.source = source;
+    }
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Node destination) {
+        this.destination = destination;
+    }
+
+    public Set<Node> getSourcesConnections() {
+        return sourcesConnections;
+    }
+
+    public boolean isCallback() {
+        return callback;
+    }
+
+    public void setCallback(boolean callback) {
+        this.callback = callback;
+    }
+}
